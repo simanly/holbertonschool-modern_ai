dc6dc28fcc1c2897a6bcf437a103aabe7c437422
Sofiya <imanlisofi@gmail.com>
2026-06-30 18:18:31 +0000

    Task 1: PCA

--- machine_learning/unsupervised_learning/1-pca.py
+++ machine_learning/unsupervised_learning/1-pca.py
@@ -26,5 +26,5 @@ def Apply_PCA(X, n_components, random_state):
     pca = decomposition.PCA(n_components=n_components,
                             random_state=random_state)
     X_pca = pca.fit_transform(X)
-    
+
     return X_pca, pca
